a1a5650a04a3b46719887fa69116a62085fa1114
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2022-10-28 10:57:04 +0530

    default compute.sans to empty dict
    compute_sans is defaulted to string of
    empty dictionary. Change it to just an
    empty dictionary.

--- sunbeam/hooks.py
+++ sunbeam/hooks.py
@@ -22,7 +22,7 @@ LOG = logging.getLogger(__name__)
 DEFAULT_CONFIG = {
     "control-plane.cloud": "microk8s",
     "control-plane.model": "openstack",
-    "compute.sans": "{}",
+    "compute.sans": {},
     "node.role": "CONVERGED",
     "snap.channel.juju": "3.0/stable",
     "snap.channel.microk8s": "1.25-strict/stable",
